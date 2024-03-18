3cc1c4d5fa2f32c4cf65f61b5f3e19a4dace3c4a
Jason Falk <jasonfalk9@gmail.com>
2024-03-18 14:52:07 -0700

    Add course model.

---
+++ UniversityRegistrar/Models/Course.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace UniversityRegistrar.Models
+{
+  public class Course
+  {
+    public int CourseId { get; set; }
+    public string CourseName { get; set; }
+    public int CourseNumber { get; set; }
+  }
+}
