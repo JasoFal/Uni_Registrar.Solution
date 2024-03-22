ffa47123ebe45996d4557b7179ac7a157e533023
Jason Falk <jasonfalk9@gmail.com>
2024-03-22 15:20:09 -0700

    Add validation properties.

--- UniversityRegistrar/Models/Course.cs
+++ UniversityRegistrar/Models/Course.cs
@@ -1,12 +1,19 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace UniversityRegistrar.Models
 {
   public class Course
   {
     public int CourseId { get; set; }
+
+    [Required]
+    [StringLength(100)]
     public string CourseName { get; set; }
+
+    [Required]
     public int CourseNumber { get; set; }
+
     public List<StudentCourse> JoinEntities { get; }
   }
 }
